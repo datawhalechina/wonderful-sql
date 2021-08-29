eecabaf059279143588f0b56fbc70597ccdcf670
mba1398 <26516464+mba1398@users.noreply.github.com>
2021-08-29 18:44:51 +0800

    Update test.sql

--- materials/test.sql
+++ materials/test.sql
@@ -185,7 +185,95 @@ INSERT INTO Users VALUES (12,   'No',  'driver');
 INSERT INTO Users VALUES (13,   'No',  'driver');
 
 
+-- 秋招秘籍B
+-- 练习一
+DROP TABLE IF EXISTS `score2`;
+CREATE TABLE `score2` (
+  `name` varchar(20) DEFAULT NULL,
+  `subject` varchar(20) DEFAULT NULL,
+  `score` int DEFAULT NULL
+);
 
+INSERT INTO score2 VALUES('A', 'chinese', 99);
+INSERT INTO score2 VALUES('A', 'math',    98);
+INSERT INTO score2 VALUES('A', 'english', 97);
+INSERT INTO score2 VALUES('B', 'chinese', 92);
+INSERT INTO score2 VALUES('B', 'math',    91);
+INSERT INTO score2 VALUES('B', 'english', 90);
+INSERT INTO score2 VALUES('C', 'chinese', 88);
+INSERT INTO score2 VALUES('C', 'math',    87);
+INSERT INTO score2 VALUES('C', 'english', 86);
+
+
+-- 练习二
+DROP TABLE IF EXISTS `score22`;
+CREATE TABLE `score22` (
+  `name` varchar(20) DEFAULT NULL,
+  `chinese` int DEFAULT NULL,
+  `math`    int DEFAULT NULL,
+  `english` int DEFAULT NULL
+);
+
+INSERT INTO score22 VALUES('A', 99, 98, 97);
+INSERT INTO score22 VALUES('B', 92, 91, 90);
+INSERT INTO score22 VALUES('C', 88, 87, 86);
+
+-- 练习三
+DROP TABLE if EXISTS anchor_sales;
+CREATE TABLE anchor_sales
+(anchor_name  VARCHAR(20),
+date INT,
+sales INT);
+
+INSERT INTO anchor_sales VALUES('A', 20210101, 40000);
+INSERT INTO anchor_sales VALUES('B', 20210101, 80000);
+INSERT INTO anchor_sales VALUES('A', 20210102, 10000);
+INSERT INTO anchor_sales VALUES('C', 20210102, 90000);
+INSERT INTO anchor_sales VALUES('A', 20210103,  7500);
+INSERT INTO anchor_sales VALUES('C', 20210103, 80000);
+
+
+-- 秋招秘籍C
+-- 练习一
+DROP TABLE IF EXISTS row_col;
+create table row_col 
+(cdate date,
+result varchar(255));
+
+
+insert into row_col values ('2021-01-01','胜');
+insert into row_col values ('2021-01-01','负');
+insert into row_col values ('2021-01-03','胜');
+insert into row_col values ('2021-01-03','负');
+insert into row_col values ('2021-01-01','胜');
+insert into row_col values ('2021-01-03','负');
+
+
+-- 练习二
+DROP TABLE IF EXISTS col_row;
+create table col_row 
+(`比赛日期` date
+,`胜` int
+,`负` int);
+
+insert into col_row values ('2021-01-01', 2, 1);
+insert into col_row values ('2021-01-03', 1, 2);
+
+-- 练习三
+DROP TABLE if EXISTS t_act_records;
+CREATE TABLE t_act_records
+(uid  VARCHAR(20),
+imp_date DATE);
+
+INSERT INTO t_act_records VALUES('u1001', 20210101);
+INSERT INTO t_act_records VALUES('u1002', 20210101);
+INSERT INTO t_act_records VALUES('u1003', 20210101);
+INSERT INTO t_act_records VALUES('u1003', 20210102);
+INSERT INTO t_act_records VALUES('u1004', 20210101);
+INSERT INTO t_act_records VALUES('u1004', 20210102);
+INSERT INTO t_act_records VALUES('u1004', 20210103);
+INSERT INTO t_act_records VALUES('u1004', 20210104);
+INSERT INTO t_act_records VALUES('u1004', 20210105);
 
 
 
